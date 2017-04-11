cc8c66e1f25358103994eb0f16c331735b40320b
1_am_a_geek <tmy0x3@icloud.com>
2017-04-11 17:37:33 +0900

    Initial Commit

---
+++ Sumo/Sumo.h
@@ -0,0 +1,19 @@
+//
+//  Sumo.h
+//  Sumo
+//
+//  Created by 1amageek on 2017/04/11.
+//  Copyright © 2017年 Stamp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sumo.
+FOUNDATION_EXPORT double SumoVersionNumber;
+
+//! Project version string for Sumo.
+FOUNDATION_EXPORT const unsigned char SumoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sumo/PublicHeader.h>
+
+
---
+++ SumoTests/SumoTests.swift
@@ -0,0 +1,36 @@
+//
+//  SumoTests.swift
+//  SumoTests
+//
+//  Created by 1amageek on 2017/04/11.
+//  Copyright © 2017年 Stamp Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Sumo
+
+class SumoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
